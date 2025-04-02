49086b84ac6c0bebf938f7ddcd6c4c3444c9126d
yunseong <ttttma2@naver.com>
2025-04-03 04:02:35 +0900

    root

---
+++ src/main/java/com/lms/attendance/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.lms.attendance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // React 라우팅을 index.html로 전달
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
+}
